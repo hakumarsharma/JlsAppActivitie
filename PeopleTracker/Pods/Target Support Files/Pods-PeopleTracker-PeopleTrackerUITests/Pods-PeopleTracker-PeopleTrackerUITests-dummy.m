2e0f73e5333601f40e2e0c624f87cc6ebfe33f00
TejaSree <teja.marthy@radisy.com>
2020-04-01 17:32:51 +0530

    Change in file structure

--- PeopleTracker/Pods/Target Support Files/Pods-PeopleTracker-PeopleTrackerUITests/Pods-PeopleTracker-PeopleTrackerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PeopleTracker_PeopleTrackerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PeopleTracker_PeopleTrackerUITests
-@end
